9274cf7a736136b355f0cede35d4ce25e4e41085
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-10-27 22:05:04 +0300

    hover to about me part added

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe() {
         <div className="col-12 col-xl-5 about-me-logo"></div>
         <div className="col-12 col-xl-7 about-me-description">
           <h1>
-            About <span>Me</span>
+            About <span className="me" >Me</span>
           </h1>
           <h3>Hello! I'm Vadim Šaranov</h3>
           <p>
@@ -31,18 +31,8 @@ function AboutMe() {
           <span><i class="fa fa-linkedin fa-lg"></i></span>
         </div>
           </div>
-            {/* <a class="footer-button" href="#">
-              <i class="fa fa-github fa-lg"></i>
-            </a>
-            <a class="footer-button" href="#">
-              <i class="fa fa-instagram fa-lg"></i>
-            </a>
-            <a class="footer-button" href="#">
-              <i class="fa fa-linkedin fa-lg"></i>
-            </a> */}
-
           <a href="">
-            <button>Contact me</button>
+          <button class="contact-me-button color-9">Contact Me</button>
           </a>
         </div>
       </div>
